bfeafdfc9d46c2f650453cb8a957a7883c6cd02f
Atsutomo Tabata <atabata@yahoo-corp.jp>
2021-11-29 20:54:59 +0900

    finish chap13

---
+++ chap13/minigrep/src/lib.rs
@@ -0,0 +1,119 @@
+use std::fs::File;
+use std::io::prelude::*;
+use std::error::Error;
+use std::env;
+
+pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let mut f = File::open(config.filename)?;
+
+    let mut contents = String::new();
+    f.read_to_string(&mut contents)?;
+
+    let results = if config.case_sensitive {
+        search(&config.query, &contents)
+    } else {
+        search_case_insensitive(&config.query, &contents)
+    };
+
+    if results.len() == 0 {
+        println!("no matched line");
+    } else {
+        for line in results {
+            println!("{}", line);
+        }
+    }
+
+    Ok(())
+}
+
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    contents.lines()
+        .filter(|line| line.contains(query))
+        .collect()
+}
+
+pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    contents.lines()
+        .filter(|line| line.to_lowercase().contains(query.to_lowercase()))
+        .collect()
+}
+
+pub struct Config {
+    pub query: String,
+    pub filename: String,
+    pub case_sensitive: bool,
+}
+
+impl Config {
+    pub fn new(mut args: std::env::Args) -> Result<Config, &'static str> {
+        args.next();
+
+        let query = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a query string"),
+        };
+
+        let filename = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a file name"),
+        };
+
+        let case_sensitive = env::var("CASE_INSENSITIVE").is_err();
+
+        Ok(Config {
+            query,
+            filename,
+            case_sensitive,
+        })
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    #[test]
+    fn can_parse_args() {
+        let args = vec!["command".to_string(), "query".to_string(), "filename".to_string()];
+        let config = Config::new(&args).unwrap();
+
+        assert_eq!(args[1], config.query);
+        assert_eq!(args[2], config.filename);
+    }
+
+    #[test]
+    #[should_panic(expected = "not enough arguments")]
+    fn cannot_parse_args_with_few_numbers() {
+        let args = vec!["command".to_string()];
+        let _ = Config::new(&args).unwrap();
+    }
+
+    #[test]
+    fn case_sensitive() {
+        let query = "duct";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.
+Duct tape.";
+
+        assert_eq!(
+            vec!["safe, fast, productive."],
+            search(query, contents)
+        );
+    }
+
+    #[test]
+    fn case_insensitive() {
+        let query = "rUsT";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.
+Trust me.";
+
+        assert_eq!(
+            vec!["Rust:", "Trust me."],
+            search_case_insensitive(query, contents)
+        );
+    }
+}
