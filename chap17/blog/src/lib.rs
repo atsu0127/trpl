421cf31257204d1d91b638d763af58720a9e969f
Atsutomo Tabata <atsu.dq9@gmail.com>
2021-12-19 19:14:56 +0900

    [WIP] doing chap17, remain last challenge

---
+++ chap17/blog/src/lib.rs
@@ -0,0 +1,106 @@
+pub struct Post {
+    state: Option<Box<dyn State>>,
+    content: String,
+}
+
+impl Post {
+    pub fn new() -> Post {
+        Post {
+            state: Some(Box::new(Draft{ count: 0 })),
+            content: String::new(),
+        }
+    }
+
+    pub fn add_text(&mut self, test: &str) {
+        self.content.push_str(test);
+    }
+
+    pub fn content(&self) -> &str {
+        self.state.as_ref().unwrap().content(&self)
+    }
+
+    pub fn request_review(&mut self) {
+        if let Some(s) = self.state.take() {
+            self.state = Some(s.request_review());
+        }
+    }
+
+    pub fn approve(&mut self) {
+        if let Some(s) = self.state.take() {
+            self.state = Some(s.approve())
+        }
+    }
+
+    pub fn reject(&mut self) {
+        if let Some(s) = self.state.take() {
+            self.state = Some(s.reject())
+        }
+    }
+}
+
+trait State {
+    fn request_review(self: Box<Self>) -> Box<dyn State>;
+    fn approve(self: Box<Self>) -> Box<dyn State>;
+    fn reject(self: Box<Self>) -> Box<dyn State>;
+    fn content<'a>(&self, post: &'a Post) -> &'a str {
+        ""
+    }
+}
+
+struct Draft {
+    count: u8,
+}
+
+impl State for Draft {
+    fn request_review(self: Box<Self>) -> Box<dyn State> {
+        if self.count == 1 {
+            Box::new(PendingReview {})
+        } else {
+            Box::new(Draft { count: self.count + 1 })
+        }
+    }
+
+    fn approve(self: Box<Self>) -> Box<dyn State> {
+        self
+    }
+
+    fn reject(self: Box<Self>) -> Box<dyn State> {
+        self
+    }
+}
+
+struct PendingReview {}
+
+impl State for PendingReview {
+    fn request_review(self: Box<Self>) -> Box<dyn State> {
+        self
+    }
+
+    fn approve(self: Box<Self>) -> Box<dyn State> {
+        Box::new(Published {})
+    }
+
+    fn reject(self: Box<Self>) -> Box<dyn State> {
+        Box::new(Draft { count: 0 })
+    }
+}
+
+struct Published {}
+
+impl State for Published {
+    fn request_review(self: Box<Self>) -> Box<dyn State> {
+        self
+    }
+
+    fn approve(self: Box<Self>) -> Box<dyn State> {
+        self
+    }
+
+    fn reject(self: Box<Self>) -> Box<dyn State> {
+        self
+    }
+
+    fn content<'a>(&self, post: &'a Post) -> &'a str {
+        &post.content
+    }
+}
--- chap17/blog/src/main.rs
+++ chap17/blog/src/main.rs
@@ -2,17 +2,53 @@ extern crate blog;
 use blog::Post;
 
 fn main() {
+    // 正常系
     let mut post = Post::new();
 
     // 草稿
     post.add_text("I ate a salad for lunch today");
     assert_eq!("", post.content());
 
-    // 査読
+    // 査読(1回目)
+    post.request_review();
+    assert_eq!("", post.content());
+
+    // 査読(2回目)
     post.request_review();
     assert_eq!("", post.content());
 
     // 公開
     post.approve();
-    assert_eq!("I ate a salad for lunch today", post.content);
+    assert_eq!("I ate a salad for lunch today", post.content());
+
+    // reject確認
+    let mut post = Post::new();
+
+    // 草稿
+    post.add_text("It will reject");
+    assert_eq!("", post.content());
+
+    // 査読
+    post.request_review();
+    post.request_review();
+    assert_eq!("", post.content());
+
+    // リジェクト
+    post.reject();
+    assert_eq!("", post.content());
+
+    // 一回じゃ公開できない
+    let mut post = Post::new();
+
+    // 草稿
+    post.add_text("I ate a salad for lunch today");
+    assert_eq!("", post.content());
+
+    // 査読(1回目)
+    post.request_review();
+    assert_eq!("", post.content());
+
+    // 公開
+    post.approve();
+    assert_eq!("", post.content());
 }
